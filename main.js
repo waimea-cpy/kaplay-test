bdcca1919a4ca5ba3b5d6484d19444bfd579134b
Steve Copley <steve.copley@waimea.school.nz>
2026-08-06 21:37:20 +1200

    Splash screen added

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs"
+
+import { createSplashScene } from "./scenes/splash.js"
 import { createGameScene } from "./scenes/game.js"
 import { createGameOverScene } from "./scenes/gameOver.js"
 
 kaplay({ background: [20, 20, 30] })
 
+createSplashScene()
 createGameScene()
 createGameOverScene()
 
-go("game")
+go("splash")
 
